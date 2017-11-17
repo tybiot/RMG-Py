7c1b31274c92897e0692cca9838ff09fd23d8917
alongd <alongd@mit.edu>
2017-11-17 17:33:10 -0500

    Avoid making carbenes in findAllDelocalizationPathsN5dd_N5ts
    Regardless of the direction in generateN5dd_N5tsResonanceStructures(),
    atom2 is always the one onto which the incrementLonePairs() action is
    applied. This commit verifies that atom2 is not carbon so no carbenes
    are formed.

--- rmgpy/molecule/pathfinder.py
+++ rmgpy/molecule/pathfinder.py
@@ -303,7 +303,8 @@ def findAllDelocalizationPathsN5dd_N5ts(atom1):
             for atom3, bond13 in atom1.edges.items():
                 index_atom_3 = index_atom_3 + 1
                 # Only double bonds are considered, at the moment we only consider non-radical nitrogen and oxygen atoms
-                if (bond13.isDouble() and atom3.radicalElectrons == 0 and atom3.lonePairs > 0 and not atom3.isOxygen() and not atom3.isCarbon() and (index_atom_2 != index_atom_3)):
+                if (bond13.isDouble() and atom3.radicalElectrons == 0 and atom3.lonePairs > 0 and not atom3.isOxygen()
+                    and not atom3.isCarbon() and not atom2.isCarbon() and (index_atom_2 != index_atom_3)):
                     paths.append([atom1, atom2, atom3, bond12, bond13, 1])
     
     for atom2, bond12 in atom1.edges.items():
@@ -311,7 +312,8 @@ def findAllDelocalizationPathsN5dd_N5ts(atom1):
         if bond12.isTriple():
             for atom3, bond13 in atom1.edges.items():
                 # Only single bonds are considered, at the moment we only consider negatively charged nitrogen and oxygen
-                if (bond13.isSingle() and ((atom3.isNitrogen() and atom3.lonePairs >= 2) or (atom3.isOxygen() and atom3.lonePairs >= 3))):
+                if (bond13.isSingle() and ((atom3.isNitrogen() and atom3.lonePairs >= 2) or (atom3.isOxygen()
+                        and atom3.lonePairs >= 3)) and not atom2.isCarbon()):
                     paths.append([atom1, atom2, atom3, bond12, bond13, 2])
     
     return paths    
